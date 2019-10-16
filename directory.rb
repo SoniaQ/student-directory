fb66ca52e5ebf9b02a1a287222cdb88f2563eda8
Sonia Quintiao <soniaquintiao@gmail.com>
2019-10-16 16:29:53 +0100

    updated code to include methods with calls to improve readability and execution

--- directory.rb
+++ directory.rb
@@ -1,8 +1,5 @@
-student_count = 11
 # let's put all students into an array
 students = [
-  "The students of Villains Academy",
-  "----------",
   "Dr. Hannibal Lecter",
   "Darth Vader",
   "Nurse Ratched",
@@ -13,11 +10,24 @@ students = [
   "Freddy Krueger",
   "The Joker",
   "Joffrey Baratheon",
-  "Norman Bates",
+  "Norman Bates"
 ]
-# and then print them
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------"
 end
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
